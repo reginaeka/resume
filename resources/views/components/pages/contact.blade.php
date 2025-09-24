b52cd9e274da59e96a0718a8385867d6d0b673a3
Herayadi <heri.rahmat00@gmail.com>
2025-09-24 22:42:18 +0700

    update: - header scroller transparent to 1 - efek underline navbar - paragraf contact

--- resources/views/components/pages/contact.blade.php
+++ resources/views/components/pages/contact.blade.php
@@ -1,98 +1,100 @@
     <!-- Page Title -->
     <div class="page-title" data-aos="fade">
-      <div class="heading">
-        <div class="container">
-          <div class="row d-flex justify-content-center text-center">
-            <div class="col-lg-8">
-              <h1>Contact</h1>
-              <!-- <p class="mb-0">Odio et unde deleniti. Deserunt numquam exercitationem. Officiis quo odio sint voluptas consequatur ut a odio voluptatem. Sit dolorum debitis veritatis natus dolores. Quasi ratione sint. Sit quaerat ipsum dolorem.</p> -->
+        <div class="heading">
+            <div class="container">
+                <div class="row d-flex justify-content-center text-center">
+                    <div class="col-lg-8">
+                        <h1>Contact</h1>
+                        <p class="mb-0">Reach out — whether it’s a project, a question, or just to say hello.</p>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-      <nav class="breadcrumbs">
-        <div class="container">
-          <ol>
-            <li><a href="/">Home</a></li>
-            <li class="current">Contact</li>
-          </ol>
-        </div>
-      </nav>
+        <nav class="breadcrumbs">
+            <div class="container">
+                <ol>
+                    <li><a href="/">Home</a></li>
+                    <li class="current">Contact</li>
+                </ol>
+            </div>
+        </nav>
     </div><!-- End Page Title -->
 
     <!-- Contact Section -->
     <section id="contact" class="contact section">
+        <div class="container" data-aos="fade-up" data-aos-delay="100">
+            <div class="row gy-4">
+                <div class="col-md-4 responsive-col">
+                    <a href="https://maps.app.goo.gl/kdCB9gXErsdMsmR1A">
+                        <div class="info-item d-flex align-items-center" data-aos="fade-up" data-aos-delay="200">
+                            <i class="icon bi bi-geo-alt flex-shrink-0"></i>
+                            <div>
+                                <h3>Address</h3>
+                                <p>Jakarta Selatan, DKI Jakarta, Indonesia</p>
+                            </div>
+                        </div>
+                    </a>
+                </div><!-- End Info Item -->
 
-      <div class="container" data-aos="fade-up" data-aos-delay="100">
+                <div class="col-md-4 responsive-col">
+                    <a href="mailto:ekaregina846@gmail.com">
+                        <div class="info-item d-flex align-items-center" data-aos="fade-up" data-aos-delay="400">
+                            <i class="icon bi bi-envelope flex-shrink-0"></i>
+                            <div>
+                                <h3>Email Us</h3>
+                                <p>ekaregina846@gmail.com</p>
+                            </div>
+                        </div>
+                    </a>
+                </div><!-- End Info Item -->
 
-        <div class="row gy-4">
-          <div class="col-md-4 responsive-col">
-            <a href="https://maps.app.goo.gl/kdCB9gXErsdMsmR1A">
-            <div class="info-item d-flex align-items-center" data-aos="fade-up" data-aos-delay="200">
-              <i class="icon bi bi-geo-alt flex-shrink-0"></i>
-              <div>
-                <h3>Address</h3>
-                <p>Jakarta Selatan, DKI Jakarta, Indonesia</p>
-              </div>
+                <div class="col-md-4 responsive-col">
+                    <a href="https://www.linkedin.com/in/regina-septianad/">
+                        <div class="info-item d-flex align-items-center" data-aos="fade-up" data-aos-delay="500">
+                            <i class="icon bi bi-linkedin flex-shrink-0"></i>
+                            <div>
+                                <h3>LinkedIn</h3>
+                                <p>Regina Eka Septianadrah</p>
+                            </div>
+                        </div>
+                    </a>
+                </div><!-- End Info Item -->
+
+
+                <form action="forms/contact.php" method="post" class="php-email-form" data-aos="fade-up"
+                    data-aos-delay="600">
+                    <div class="row gy-4">
+
+                        <div class="col-md-6">
+                            <input type="text" name="name" class="form-control" placeholder="Your Name"
+                                required="">
+                        </div>
+
+                        <div class="col-md-6 ">
+                            <input type="email" class="form-control" name="email" placeholder="Your Email"
+                                required="">
+                        </div>
+
+                        <div class="col-md-12">
+                            <input type="text" class="form-control" name="subject" placeholder="Subject"
+                                required="">
+                        </div>
+
+                        <div class="col-md-12">
+                            <textarea class="form-control" name="message" rows="6" placeholder="Message" required=""></textarea>
+                        </div>
+
+                        <div class="col-md-12 text-center">
+                            <div class="loading">Loading</div>
+                            <div class="error-message"></div>
+                            <div class="sent-message">Your message has been sent. Thank you!</div>
+
+                            <button type="submit">Send Message</button>
+                        </div>
+
+                    </div>
+                </form><!-- End Contact Form -->
+
             </div>
-          </a>
-          </div><!-- End Info Item -->
-
-          <div class="col-md-4 responsive-col">
-          <a href="mailto:ekaregina846@gmail.com">
-            <div class="info-item d-flex align-items-center" data-aos="fade-up" data-aos-delay="400">
-              <i class="icon bi bi-envelope flex-shrink-0"></i>
-              <div>
-                <h3>Email Us</h3>
-                <p>ekaregina846@gmail.com</p>
-              </div>
-            </div>
-          </a>
-        </div><!-- End Info Item -->
-
-        <div class="col-md-4 responsive-col">
-          <a href="https://www.linkedin.com/in/regina-septianad/">
-            <div class="info-item d-flex align-items-center" data-aos="fade-up" data-aos-delay="500">
-              <i class="icon bi bi-linkedin flex-shrink-0"></i>
-              <div>
-                <h3>LinkedIn</h3>
-                <p>Regina Eka Septianadrah</p>
-              </div>
-            </div>
-          </a>
-        </div><!-- End Info Item -->
-      
-
-        <form action="forms/contact.php" method="post" class="php-email-form" data-aos="fade-up" data-aos-delay="600">
-          <div class="row gy-4">
-
-            <div class="col-md-6">
-              <input type="text" name="name" class="form-control" placeholder="Your Name" required="">
-            </div>
-
-            <div class="col-md-6 ">
-              <input type="email" class="form-control" name="email" placeholder="Your Email" required="">
-            </div>
-
-            <div class="col-md-12">
-              <input type="text" class="form-control" name="subject" placeholder="Subject" required="">
-            </div>
-
-            <div class="col-md-12">
-              <textarea class="form-control" name="message" rows="6" placeholder="Message" required=""></textarea>
-            </div>
-
-            <div class="col-md-12 text-center">
-              <div class="loading">Loading</div>
-              <div class="error-message"></div>
-              <div class="sent-message">Your message has been sent. Thank you!</div>
-
-              <button type="submit">Send Message</button>
-            </div>
-
-          </div>
-        </form><!-- End Contact Form -->
-
-      </div>
 
     </section><!-- /Contact Section -->
--- resources/views/components/partials/header.blade.php
+++ resources/views/components/partials/header.blade.php
@@ -1,23 +1,30 @@
  <header id="header" class="header d-flex align-items-center fixed-top">
-    <div class="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
+     <div class="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
 
-      <a href="/" class="logo d-flex align-items-center">
-        <!-- Uncomment the line below if you also wish to use an image logo -->
-        <!-- <img src="assets/img/logo.webp" alt=""> -->
-        <h1 class="sitename">Resume</h1>
-      </a>
+         <a href="/" class="logo d-flex align-items-center">
+             <!-- Uncomment the line below if you also wish to use an image logo -->
+             <!-- <img src="assets/img/logo.webp" alt=""> -->
+             <h1 class="sitename">Resume</h1>
+         </a>
 
-      <nav id="navmenu" class="navmenu">
-        <ul>
-          <li><a href="/" class="active">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/resume">Resume</a></li>
-          <li><a href="/services">Services</a></li>
-          <li><a href="/portfolio">Portfolio</a></li>
-          <li><a href="/contact">Contact</a></li>
-        </ul>
-        <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
-      </nav>
+         {{-- @dd(Request::is('/')) --}}
 
-    </div>
-  </header>
+         {{-- @php
+             $request = request()->getRequestUri();
+         @endphp --}}
+
+
+         <nav id="navmenu" class="navmenu">
+             <ul>
+                 <li><a href="/" class="{{ Request::is('/') ? 'active' : '' }}">Home</a></li>
+                 <li><a href="/about" class="{{ Request::is('about') ? 'active' : '' }}">About</a></li>
+                 <li><a href="/resume" class="{{ Request::is('resume') ? 'active' : '' }}">Resume</a></li>
+                 <li><a href="/services" class="{{ Request::is('services') ? 'active' : '' }}">Services</a></li>
+                 <li><a href="/portfolio" class="{{ Request::is('portofolio') ? 'active' : '' }}">Portfolio</a></li>
+                 <li><a href="/contact" class="{{ Request::is('contact') ? 'active' : '' }}">Contact</a></li>
+             </ul>
+             <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+         </nav>
+
+     </div>
+ </header>
