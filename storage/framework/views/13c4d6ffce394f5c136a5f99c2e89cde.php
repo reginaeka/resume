2edc6fd6950858a0f8366b87a342019fcf879a4e
Herayadi <heri.rahmat00@gmail.com>
2025-09-25 22:03:41 +0700

    ganti icon

--- storage/framework/views/13c4d6ffce394f5c136a5f99c2e89cde.php
+++ storage/framework/views/13c4d6ffce394f5c136a5f99c2e89cde.php
@@ -1,43 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <title>Regina Septianadrah - Resume</title>
-  <meta name="description" content="">
-  <meta name="keywords" content="">
+    <head>
+        <meta charset="utf-8">
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+        <title>Regina Septianadrah - Resume</title>
+        <meta name="description" content="">
+        <meta name="keywords" content="">
 
-  <!-- Favicons -->
-  <link href="<?php echo e(asset('assets/img/favicon.png')); ?>" rel="icon">
-  <link href="<?php echo e(asset('assets/img/apple-touch-icon.png')); ?>" rel="apple-touch-icon">
+        <!-- Favicons -->
+        <link href="<?php echo e(asset('assets/img/icon.png')); ?>" rel="icon">
+        <link href="<?php echo e(asset('assets/img/icon.png')); ?>" rel="icon">
 
-  <!-- Fonts -->
-  <link href="https://fonts.googleapis.com" rel="preconnect">
-  <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com" rel="preconnect">
+        <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+        <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+            rel="stylesheet">
 
-  <!-- Vendor CSS Files -->
-  <link href="<?php echo e(asset('assets/vendor/bootstrap/css/bootstrap.min.css')); ?>" rel="stylesheet">
-  <link href="<?php echo e(asset('assets/vendor/bootstrap-icons/bootstrap-icons.css')); ?>" rel="stylesheet">
-  <link href="<?php echo e(asset('assets/vendor/aos/aos.css')); ?>" rel="stylesheet">
-  <link href="<?php echo e(asset('assets/vendor/swiper/swiper-bundle.min.css')); ?>" rel="stylesheet">
-  <link href="<?php echo e(asset('assets/vendor/glightbox/css/glightbox.min.css')); ?>" rel="stylesheet">
+        <!-- Vendor CSS Files -->
+        <link href="<?php echo e(asset('assets/vendor/bootstrap/css/bootstrap.min.css')); ?>" rel="stylesheet">
+        <link href="<?php echo e(asset('assets/vendor/bootstrap-icons/bootstrap-icons.css')); ?>" rel="stylesheet">
+        <link href="<?php echo e(asset('assets/vendor/aos/aos.css')); ?>" rel="stylesheet">
+        <link href="<?php echo e(asset('assets/vendor/swiper/swiper-bundle.min.css')); ?>" rel="stylesheet">
+        <link href="<?php echo e(asset('assets/vendor/glightbox/css/glightbox.min.css')); ?>" rel="stylesheet">
 
-  <!-- Main CSS File -->
-  <link href="<?php echo e(asset('assets/css/main.css')); ?>" rel="stylesheet">
+        <!-- Main CSS File -->
+        <link href="<?php echo e(asset('assets/css/main.css')); ?>" rel="stylesheet">
 
-  <!-- =======================================================
+        <!-- =======================================================
   * Template Name: Personal
   * Template URL: https://bootstrapmade.com/personal-free-resume-bootstrap-template/
   * Updated: Mar 05 2025 with Bootstrap v5.3.3
   * Author: BootstrapMade.com
   * License: https://bootstrapmade.com/license/
   ======================================================== -->
-</head>
+    </head>
 
-<body class="index-page">
-    <?php if (isset($component)) { $__componentOriginal21b90aee62eb729a70bc5150038e88d2 = $component; } ?>
+    <body class="index-page">
+        <?php if (isset($component)) { $__componentOriginal21b90aee62eb729a70bc5150038e88d2 = $component; } ?>
 <?php if (isset($attributes)) { $__attributesOriginal21b90aee62eb729a70bc5150038e88d2 = $attributes; } ?>
 <?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.partials.header','data' => []] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
 <?php $component->withName('partials.header'); ?>
@@ -56,12 +58,12 @@
 <?php if (isset($__componentOriginal21b90aee62eb729a70bc5150038e88d2)): ?>
 <?php $component = $__componentOriginal21b90aee62eb729a70bc5150038e88d2; ?>
 <?php unset($__componentOriginal21b90aee62eb729a70bc5150038e88d2); ?>
-<?php endif; ?> 
-  
-    <?php echo e($slot); ?>
+<?php endif; ?>
+
+        <?php echo e($slot); ?>
 
 
-  <?php if (isset($component)) { $__componentOriginal3bcc21be352e90c327647659b100c2a9 = $component; } ?>
+        <?php if (isset($component)) { $__componentOriginal3bcc21be352e90c327647659b100c2a9 = $component; } ?>
 <?php if (isset($attributes)) { $__attributesOriginal3bcc21be352e90c327647659b100c2a9 = $attributes; } ?>
 <?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.partials.footer','data' => []] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
 <?php $component->withName('partials.footer'); ?>
@@ -82,27 +84,29 @@
 <?php unset($__componentOriginal3bcc21be352e90c327647659b100c2a9); ?>
 <?php endif; ?>
 
-  <!-- Scroll Top -->
-  <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+        <!-- Scroll Top -->
+        <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i
+                class="bi bi-arrow-up-short"></i></a>
 
-  <!-- Preloader -->
-  <div id="preloader"></div>
+        <!-- Preloader -->
+        <div id="preloader"></div>
 
-  <!-- Vendor JS Files -->
-  <script src="<?php echo e(asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')); ?>"></script>
-  <script src="<?php echo e(asset('assets/vendor/php-email-form/validate.js')); ?>"></script>
-  <script src="<?php echo e(asset('assets/vendor/aos/aos.js')); ?>"></script>
-  <script src="<?php echo e(asset('assets/vendor/typed.js/typed.umd.js')); ?>"></script>
-  <script src="<?php echo e(asset('assets/vendor/purecounter/purecounter_vanilla.js')); ?>"></script>
-  <script src="<?php echo e(asset('assets/vendor/waypoints/noframework.waypoints.js')); ?>"></script>
-  <script src="<?php echo e(asset('assets/vendor/swiper/swiper-bundle.min.js')); ?>"></script>
-  <script src="<?php echo e(asset('assets/vendor/glightbox/js/glightbox.min.js')); ?>"></script>
-  <script src="<?php echo e(asset('assets/vendor/imagesloaded/imagesloaded.pkgd.min.js')); ?>"></script>
-  <script src="<?php echo e(asset('assets/vendor/isotope-layout/isotope.pkgd.min.js')); ?>"></script>
+        <!-- Vendor JS Files -->
+        <script src="<?php echo e(asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')); ?>"></script>
+        <script src="<?php echo e(asset('assets/vendor/php-email-form/validate.js')); ?>"></script>
+        <script src="<?php echo e(asset('assets/vendor/aos/aos.js')); ?>"></script>
+        <script src="<?php echo e(asset('assets/vendor/typed.js/typed.umd.js')); ?>"></script>
+        <script src="<?php echo e(asset('assets/vendor/purecounter/purecounter_vanilla.js')); ?>"></script>
+        <script src="<?php echo e(asset('assets/vendor/waypoints/noframework.waypoints.js')); ?>"></script>
+        <script src="<?php echo e(asset('assets/vendor/swiper/swiper-bundle.min.js')); ?>"></script>
+        <script src="<?php echo e(asset('assets/vendor/glightbox/js/glightbox.min.js')); ?>"></script>
+        <script src="<?php echo e(asset('assets/vendor/imagesloaded/imagesloaded.pkgd.min.js')); ?>"></script>
+        <script src="<?php echo e(asset('assets/vendor/isotope-layout/isotope.pkgd.min.js')); ?>"></script>
 
-  <!-- Main JS File -->
-  <script src="<?php echo e(asset('assets/js/main.js')); ?>"></script>
+        <!-- Main JS File -->
+        <script src="<?php echo e(asset('assets/js/main.js')); ?>"></script>
 
-</body>
+    </body>
 
-</html><?php /**PATH D:\project resume\resume\resources\views/components/layout.blade.php ENDPATH**/ ?>
+</html>
+<?php /**PATH D:\project resume\resume\resources\views/components/layout.blade.php ENDPATH**/ ?>
