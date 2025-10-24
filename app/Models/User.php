3040faf180d1d5ce8543980105c58a6a1972f5e2
Herayadi <heri.rahmat00@gmail.com>
2025-10-24 23:01:00 +0700

    create migration. create models

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,7 +20,11 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'password',
+        'phoneNum',
+        'dob',
+        'summary',
+        'website',
+        'city',
     ];
 
     /**
